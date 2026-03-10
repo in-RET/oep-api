cf281c448993b815fb20df8411777a779725aef1
Andreas Lubojanski <andreas@lubojanski.com>
2026-03-10 16:05:47 +0100

    [update] Updated the data folder

--- standalone.py
+++ standalone.py
@@ -2,8 +2,6 @@ import json
 import os
 import sys
 
-from pandas.core.common import fill_missing_names
-
 from oep.main import OepUploader
 
 TOKEN = os.getenv('OEP_API_TOKEN')
@@ -11,23 +9,17 @@ TOPIC = "sandbox"
 
 print(f"OEP API Key: {TOKEN}")
 
-data = [
-    "data/demand/ghd_east.json",
-    "data/demand/ghd_middle.json",
-    "data/demand/ghd_north.json",
-    "data/demand/ghd_swest.json",
-    "data/demand/household_east.json",
-    "data/demand/household_middle.json",
-    "data/demand/household_north.json",
-    "data/demand/household_swest.json",
-    "data/demand/industry_east.json",
-    "data/demand/industry_middle.json",
-    "data/demand/industry_north.json",
-    "data/demand/industry_swest.json",
-    "data/demand/mobility.json"
-]
+upload_list = []
 
-for filename in data:
+for root, dir, files in os.walk("data"):
+    for filename in files:
+        if filename.endswith(".json"):
+            upload_list.append(os.path.join(root, filename))
+
+print(f"Uploading {len(upload_list)} files...")
+print(upload_list)
+
+for filename in upload_list:
     print(f"Filename: {filename}")
     with open(filename, "r") as f:
         json_data = json.loads(f.read())
@@ -36,25 +28,25 @@ for filename in data:
     #print(f"Table: {table_name}")
     if len(table_name) > 50:
         print(f"Table name too long: {table_name}", file=sys.stderr)
-        break
+        print(f"Length: {len(table_name)}", file=sys.stderr)
+    else:
+        primary_key = json_data['resources'][0]['schema']['primaryKey'][0]
+        #print(f"Primary Key: {primary_key}")
 
-    primary_key = json_data['resources'][0]['schema']['primaryKey'][0]
-    #print(f"Primary Key: {primary_key}")
+        delete_existing = True
 
-    delete_existing = True
-
-    #print("=" * 60)
-    #print(f"Uploading {table_name}...")
-    #print(f"Table: {table_name}")
-    try:
-        uploader = OepUploader(token=TOKEN, topic=TOPIC)
-        uploader.upload_complete(
-            data_file=filename.replace(".json", ".csv"),
-            table_name=table_name,
-            metadata_file=filename,
-            primary_key=primary_key,
-            delete_existing=delete_existing
-        )
-        print("=" * 60)
-    except Exception as e:
-        print(f"\n Error: {str(e)}", file=sys.stderr)
+        #print("=" * 60)
+        #print(f"Uploading {table_name}...")
+        #print(f"Table: {table_name}")
+        try:
+            uploader = OepUploader(token=TOKEN, topic=TOPIC)
+            uploader.upload_complete(
+                data_file=filename.replace(".json", ".csv"),
+                table_name=table_name,
+                metadata_file=filename,
+                primary_key=primary_key,
+                delete_existing=delete_existing
+            )
+            print("=" * 60)
+        except Exception as e:
+            print(f"\n Error: {str(e)}", file=sys.stderr)
